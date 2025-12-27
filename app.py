f4ffd7e222aef18c7fdd28c46d2cf9ba2acef5b9
TimurT <ofset989@gmai.com>
2025-12-27 16:40:39 +0100

    final commit

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ import json
 country_name = input("Input full country's name:")
 
 url =  f"https://restcountries.com/v3.1/name/{country_name}"
+url2 = "https://catfact.ninja/fact"
+url3 = "https://zenquotes.io/api/random"
 responce = requests.get(url)
+responce_facts1= requests.get(url2)
+responce_facts2= requests.get(url3)
+
 content_dict = responce.json()
+content_dict2 = responce_facts1.json()
+content_dict3 = responce_facts2.json()
+
 population_num= content_dict[0]["population"]
 print (f"""
        Official name: {content_dict[0]["name"]["official"]}
@@ -15,3 +23,8 @@ print (f"""
        Location: {content_dict[0]["latlng"][0]}, {content_dict[0]["latlng"][1]}
 """                                                                                                          
                                                             )
+print (f"""
+       ==FUN FACTS==
+Fact 1: {content_dict2["fact"]}
+Fact 2: {content_dict3[0]["q"]}
+""")
